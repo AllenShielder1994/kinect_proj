2871fb24e755c9b503bf64c9b06878bc679fe2d5
AllenShielder1994 <1224935003@qq.com>
2020-03-18 12:53:15 +1300

    1 st commit

---
+++ test.py
@@ -0,0 +1,106 @@
+from pykinect2 import PyKinectV2
+from pykinect2.PyKinectV2 import *
+from pykinect2 import PyKinectRuntime
+
+import numpy as np
+import cv2
+from cv2 import cv2
+
+kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
+#######################################################################
+#GREEN
+#lower_color=np.array([35,43,46])
+#upper_color=np.array([77,255,255])
+
+
+#YELLOW
+#lower_color=np.array([26,43,46])
+#upper_color=np.array([34,255,255])
+
+#RED
+lower_color=np.array([156,43,46])
+upper_color=np.array([180,255,255])
+
+#BLUE
+#lower_color=np.array([100,43,46])
+#upper_color=np.array([124,255,255])
+######################################################################
+#point_color = (0, 0, 255) #red
+#point_color = (255, 0, 0) #blue
+point_color = (0, 255, 0) #green
+
+red = (0, 0, 255) #red
+blue = (255, 0, 0) #blue
+green = (0, 255, 0) #green
+
+def show_distance (trgt_length,focallength,per_length):
+    
+    if per_length != 0:
+        distance=(trgt_length*focallength) / per_length
+        print (distance)
+    else: 
+        print ("no distance can be detacted")
+
+
+
+while not kinect.has_new_color_frame():
+    a = None
+
+while True:
+    if kinect.has_new_color_frame():
+        newImg = np.array(kinect.get_last_color_frame())
+        newImg = newImg.reshape((1080,1920,4))
+       # cv2.imshow("Kinect", newImg)
+        # DO PROCESSING
+
+        
+
+
+        
+        hsv = cv2.cvtColor(newImg, cv2.COLOR_BGR2HSV)
+
+        # get mask
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+
+        
+
+        cv2.namedWindow("Mask")
+
+
+        cv2.imshow('Mask', mask)
+
+
+        # detect color
+        res = cv2.bitwise_and(newImg, newImg, mask=mask)
+        cv2.imshow('Result', res)
+        
+        
+        #cv2.circle(newImg, positon, 60, point_color, 0)
+
+        #cv2.namedWindow("image")
+        #cv2.imshow('image', newImg) 
+
+
+        conts,hier = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)# find out edge
+        cv2.drawContours(newImg,conts,-1,point_color,1)# show contours on monitor
+        #dst = cv2.bitwise_and(newImg,newImg,mask=mask)#trake each picture
+        #cv2.imshow ("dst",dst)
+        cv2.imshow ("img_detect",newImg)
+
+        for i in range(0,len(conts)):  
+            x, y, w, h = cv2.boundingRect(conts[i])   
+            cv2.rectangle(newImg, (x,y), (x+w,y+h), green, 2) 
+
+        cv2.imshow("img",newImg)
+        #print (conts)
+
+
+
+
+    if cv2.waitKey(1) & 0xFF == ord('Q'):
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# cv2.waitKey(0)
